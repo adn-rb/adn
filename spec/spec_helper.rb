adf3024d7d137124150da9a7ac7fe153d9dffcd5
Peter Hellberg <peter@c7.se>
2012-08-29 22:32:27 +0200

    Helper methods that stub method calls to return the passed in arguments instead of actually calling the method.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,11 @@ require "find"
 %w{./lib}.each do |load_path|
   Find.find(load_path) { |f| require f if f.match(/\.rb$/) }
 end
+
+def arg(m, k = ADN, &b)
+  k.stub(m,->(a){a}, &b)
+end
+
+def args(m, k = ADN, &b)
+  k.stub(m,->(*a){a}, &b)
+end
