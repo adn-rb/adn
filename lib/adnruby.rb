bd29e22e318e27cd7adf70c7067a56ea1418609c
buddhamagnet <buddhamagnet@gmail.com>
2012-09-02 22:03:40 +0100

    corrected typo in constant

--- lib/adnruby.rb
+++ lib/adnruby.rb
@@ -33,7 +33,7 @@ module ADN
   API_HOST = "alpha-api.app.net"
   API_ENDPOINT = "/stream/0"
   API_ENDPOINT_POSTS = "#{API_ENDPOINT}/posts"
-  API_ENDPOINT_UERS = "#{API_ENDPOINT}/users"
+  API_ENDPOINT_USERS = "#{API_ENDPOINT}/users"
   HTTP = Net::HTTP.new(API_HOST, 443)
   
   HTTP.use_ssl = true
