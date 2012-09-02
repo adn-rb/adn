5fc94733a722d3d0b6ebdcc6b4ed50033fc85d9e
Peter Hellberg <peter@c7.se>
2012-09-02 23:58:31 +0200

    Spec that verifies that ADN has constants containing the API endpoints for posts and users.

--- spec/adn_spec.rb
+++ spec/adn_spec.rb
@@ -19,6 +19,11 @@ describe ADN do
     ADN::API_HOST.must_equal 'alpha-api.app.net'
   end
 
+  it "has constants containing the API endpoints for posts and users" do
+    ADN::API_ENDPOINT_POSTS.must_equal '/stream/0/posts'
+    ADN::API_ENDPOINT_USERS.must_equal '/stream/0/users'
+  end
+
   # TODO: Move into the ADN module, and rename using snake case
   #       Should probably be refactored to a separate class
   it "currently has a constant containing a http client" do
