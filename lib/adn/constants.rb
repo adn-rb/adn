ec7667c74c1f787dde34f58ec922832750c411f9
buddhamagnet <buddhamagnet@gmail.com>
2012-09-05 16:43:32 +0100

    extracted token endpoint into constant

--- lib/adn/constants.rb
+++ lib/adn/constants.rb
@@ -7,6 +7,7 @@ module ADN
   API_ENDPOINT = "/stream/0"
   API_ENDPOINT_POSTS = "#{API_ENDPOINT}/posts"
   API_ENDPOINT_USERS = "#{API_ENDPOINT}/users"
+  API_ENDPOINT_TOKEN = "#{API_ENDPOINT}/token"
   HTTP = Net::HTTP.new(API_HOST, 443)
 
   HTTP.use_ssl = true
