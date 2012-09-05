1afe9d4c4d1b423ffe5e85938fa815b623dddd20
buddhamagnet <buddhamagnet@gmail.com>
2012-09-05 12:02:55 +0100

    removed redundant references to self

--- lib/adn/post.rb
+++ lib/adn/post.rb
@@ -8,24 +8,24 @@ module ADN
 
     def self.send_post(params)
       result = ADN::API::Post.create(params)
-      new(result["data"]) unless ADN.has_error?(result)
+      Post.new(result["data"]) unless ADN.has_error?(result)
     end
 
     def initialize(raw_post)
       if raw_post.respond_to?(:each_pair)
-        self.set_values(raw_post)
+        set_values(raw_post)
         post_id = id
       else
         post_id = raw_post
-        details = self.details
-        if details.has_key? "data"
-          self.set_values(details["data"])
+        post_details = details
+        if post_details.has_key? "data"
+          set_values(post_details["data"])
         end
       end
     end
 
     def details
-      if self.id
+      if id
         Hash[self.instance_variables.map { |i| [i.to_s.slice(1..-1), self.instance_variable_get(i)]}]
       else
         ADN::API::Post.by_id(post_id)
@@ -41,23 +41,23 @@ module ADN
     end
 
     def reply_to_post
-      result = ADN::API::Post.by_id reply_to
-      Post.new(result["data"]) unless ADN.has_error?(result)
+      result = ADN::API::Post.by_id(reply_to)
+      Post.new(result["data"])
     end
 
     def replies(params = nil)
-      result = ADN::API::Post.replies(self.id, params)
+      result = ADN::API::Post.replies(id, params)
       result["data"].collect { |p| Post.new(p) } unless ADN.has_error?(result)
     end
 
     def delete
-      result = ADN::API::Post.delete(self.id)
+      result = ADN::API::Post.delete(id)
       Post.new(result["data"]) unless ADN.has_error?(result)
     end
     
     def set_values(values)
       values.each_pair do |k, v|
-        self.send("#{k}=", v) if self.respond_to?("#{k}=")
+        send("#{k}=", v) if respond_to?("#{k}=")
       end
     end
   end
