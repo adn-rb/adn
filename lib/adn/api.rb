e40d3491efb7b5a860218c643900df855b82ab9a
buddhamagnet <buddhamagnet@gmail.com>
2012-09-08 10:18:06 +0100

    added exception handling request method

--- lib/adn/api.rb
+++ lib/adn/api.rb
@@ -12,40 +12,20 @@ module ADN
       def has_error?
         has_key?("error")
       end
-
-      def invalid_request?
-        has_error? && has_value("invalid_request")
-      end
-
-      def unauthorized_client?
-        has_error && has_value("unauthorized_client")
-      end
-
-      def access_denied?
-        has_error? && has_value("access_denied")
-      end
-
-      def unsupported_response_type?
-        has_error? && has_value("unsupported_response_type")
-      end
-
-      def invalid_scope?
-        has_error? && has_value("invalid_scope")
-      end
-
-      def server_error?
-        has_error? && has_value("server_error")
-      end
-
-      def temporarily_unavailable?
-        has_error? && has_value("temporarily_unavailable")
-      end
     end
 
     def self.get_response(request)
       request.add_field("Authorization", "Bearer #{ADN.token}")
-      response = ADN::HTTP.request(request)
+      self.make_request do
+        response = ADN::HTTP.request(request)
+      end
       Response.new(JSON.parse(response.body))
+    end
+    
+    def self.make_request(&block)
+      response = block.call
+      raise ADN::APIError, response["error"] and return if response.has_error?
+      response
     end
 
     def self.get(url, params = nil)
