2c1b35b43009cf766c27f2368a47dd81d6cab20b
buddhamagnet <buddhamagnet@gmail.com>
2012-10-28 09:26:19 +0000

    modified user spec to use constants

--- spec/adn/api/user_spec.rb
+++ spec/adn/api/user_spec.rb
@@ -5,23 +5,23 @@ require_relative '../../spec_helper'
 describe ADN::API::User do
   subject { ADN::API::User }
 
-  let(:base_path) { '/stream/0/users/' }
+  let(:base_path) { ADN::API_ENDPOINT_USERS }
 
   describe "retrieve" do
     it "retrieves the user" do
-      arg(:get) { subject.retrieve(55).must_equal base_path + "55" }
+      arg(:get) { subject.retrieve(55).must_equal base_path + "/55" }
     end
   end
 
   describe "following" do
     it "retrieves the following list" do
-      arg(:get) { subject.following(44).must_equal base_path + "44/following" }
+      arg(:get) { subject.following(44).must_equal base_path + "/44/following" }
     end
   end
 
   describe "followers" do
     it "retrieves the followers list" do
-      arg(:get) { subject.following(66).must_equal base_path + "66/following" }
+      arg(:get) { subject.following(66).must_equal base_path + "/66/following" }
     end
   end
 end
