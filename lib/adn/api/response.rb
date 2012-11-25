79abbc36329ff89d81aef840a2e97534e8cff070
Leif Gensert <leif@gensert.de>
2012-11-26 00:11:52 +0100

    change error handling due to response_envelope migration

--- lib/adn/api/response.rb
+++ lib/adn/api/response.rb
@@ -6,7 +6,7 @@ module ADN
   module API
     class Response < SimpleDelegator
       def has_error?
-        has_key?("error")
+        self['meta'].nil? || self['meta']['code'].nil? || self['meta']['code'] >= 400
       end
     end
   end
--- spec/adn/api/response_spec.rb
+++ spec/adn/api/response_spec.rb
@@ -7,7 +7,7 @@ describe ADN::API::Response do
   subject { ADN::API::Response }
 
   it "currently checks a hash-like response for errors" do
-    subject.new("error" => "123").has_error?.must_equal true
-    subject.new("valid" => "123").has_error?.must_equal false
+    subject.new("meta" => {"code" => 400}).has_error?.must_equal true
+    subject.new("meta" => {"code" => 200}).has_error?.must_equal false
   end
 end
