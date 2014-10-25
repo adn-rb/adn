32b460f5f08bfe01310fbe5df48e64d5ea58c37a
Peter Hellberg <peter@c7.se>
2014-10-25 22:14:27 +0200

    Changed the host to api.app.net

--- lib/adn/constants.rb
+++ lib/adn/constants.rb
@@ -3,7 +3,7 @@
 require 'net/https'
 
 module ADN
-  API_HOST = "alpha-api.app.net"
+  API_HOST = "api.app.net"
   API_ENDPOINT = "/stream/0"
   API_ENDPOINT_POSTS = "#{API_ENDPOINT}/posts"
   API_ENDPOINT_USERS = "#{API_ENDPOINT}/users"
